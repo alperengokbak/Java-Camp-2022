99a5820f0273543c2c53066aa66c7c4729e8dc99
StrqSS <alperengokbak@gmail.com>
2022-09-21 01:10:28 +0300

    Updated FourPrecessor file, added overloading

--- src/opp/ls2RecapDemoClasses/fourProcessors.java
+++ src/opp/ls2RecapDemoClasses/fourProcessors.java
@@ -4,6 +4,13 @@ public class fourProcessors {
     public int Addition(int num1, int num2){
         return num1 + num2;
     }
+    public int Addition(int... numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
     public int Subtraction(int num1, int num2){
         return num1 - num2;
     }
